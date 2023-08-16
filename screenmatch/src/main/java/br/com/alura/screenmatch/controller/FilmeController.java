c5bc337dfd69b4c819f833d6c18f2aa9140b7155
Erick Henrique <71470639+erick-henrique-a@users.noreply.github.com>
2023-08-15 22:44:44 -0300

    Add files via upload

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,12 +1,14 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.domain.filme.DadosAlteraFilmes;
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilmes;
 import br.com.alura.screenmatch.domain.filme.Filme;
+import br.com.alura.screenmatch.domain.filme.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,24 +18,45 @@ import java.util.List;
 
 
 public class FilmeController {
-    private List<Filme> filmes = new ArrayList<>();
+    @Autowired //Spring Fica responsavel por achar e instanciar
+    private FilmeRepository repository;
     @GetMapping("formulario")
-    public String carregaPaginaFormulario(){
-
+    public String carregaPaginaFormulario(Long id, Model model){
+        if(id != null){
+            var filme = repository.getReferenceById(id);
+            model.addAttribute(filme);
+        }
         return "filmes/formulario";
     }
 
-    @GetMapping
+    @GetMapping("listagem")
     public String carregaPaginaListagem(Model model){
-        model.addAttribute("lista", filmes);
+        model.addAttribute("lista", repository.findAll());
         return "filmes/listagem";
     }
 
     @PostMapping
+    @Transactional //inicia uma transação com o banco de dados com o Spring
+
     public String cadastrafilme(DadosCadastroFilmes dados){
         Filme filme = new Filme(dados);
-        filmes.add(filme);
-        return "redirect:/filmes";
+        repository.save(filme);
+        return "redirect:/filmes/listagem";
+    }
+    @PutMapping
+    @Transactional //inicia uma transação com o banco de dados com o Spring
+    public String alterafilme(DadosAlteraFilmes dados){
+        var filme = repository.getReferenceById(dados.id());
+        filme.atualizaDados(dados);
+        return "redirect:/filmes/listagem";
+    }
+
+    @DeleteMapping
+    @Transactional //inicia uma transação com o banco de dados com o Spring
+
+    public String deletafilme(Long id){
+        repository.deleteById(id);
+        return "redirect:filmes/listagem";
     }
 
 
--- screenmatch/src/main/java/br/com/alura/screenmatch/domain/filme/Filme.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/domain/filme/Filme.java
@@ -1,27 +1,43 @@
 package br.com.alura.screenmatch.domain.filme;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "filmes")
 public class Filme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
-    private Integer duracao;
-    private Integer ano;
+    private Integer duracao_em_minutos;
+    private Integer ano_lancamento;
     private String genero;
 
 
     public Filme(DadosCadastroFilmes dados) {
         this.nome = dados.nome();
-        this.duracao = dados.duracao();
-        this.ano = dados.ano();
+        this.duracao_em_minutos = dados.duracao();
+        this.ano_lancamento = dados.ano();
         this.genero = dados.genero();
     }
+    public Filme(){}
 
     @Override
     public String toString() {
         return "Filmes{" +
                 "nome='" + nome + '\'' +
-                ", duracao=" + duracao +
-                ", ano=" + ano +
+                ", duracao=" + duracao_em_minutos +
+                ", ano=" + ano_lancamento +
                 ", genero='" + genero + '\'' +
                 '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNome() {
@@ -32,20 +48,20 @@ public class Filme {
         this.nome = nome;
     }
 
-    public Integer getDuracao() {
-        return duracao;
+    public Integer getDuracao_em_minutos() {
+        return duracao_em_minutos;
     }
 
-    public void setDuracao(Integer duracao) {
-        this.duracao = duracao;
+    public void setDuracao_em_minutos(Integer duracao_em_minutos) {
+        this.duracao_em_minutos = duracao_em_minutos;
     }
 
-    public Integer getAno() {
-        return ano;
+    public Integer getAno_lancamento() {
+        return ano_lancamento;
     }
 
-    public void setAno(Integer ano) {
-        this.ano = ano;
+    public void setAno_lancamento(Integer ano_lancamento) {
+        this.ano_lancamento = ano_lancamento;
     }
 
     public String getGenero() {
@@ -54,5 +70,12 @@ public class Filme {
 
     public void setGenero(String genero) {
         this.genero = genero;
+    }
+
+    public void atualizaDados(DadosAlteraFilmes dados) {
+        this.nome = dados.nome();
+        this.duracao_em_minutos = dados.duracao();
+        this.ano_lancamento = dados.ano();
+        this.genero = dados.genero();
     }
 }
